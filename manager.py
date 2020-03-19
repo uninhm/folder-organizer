8227e68cde161bfbe9c3db171f90a68eba7841c6
Unai Hernandez <unihernandez22@gmail.com>
2020-03-19 02:00:17 -0300

    Arreglando el error de no poner carpeta

--- manager.py
+++ manager.py
@@ -5,30 +5,36 @@ from shutil import move
 from pathlib import Path
 
 class App:
-    def __init__(self):
-        with open('folder', 'r') as f:
-            self.folder = Path(f.read())
-            f.close()
+	def __init__(self):
+		ok = False
 
-        with open('data.json', 'r') as f:
-            self.data = json.load(f)
-            f.close()
+		with open('folder', 'r') as f:
+			a = f.read()
+			if a.replace(' ', '') != '':
+				self.folder = Path(a)
+				ok = True
+			f.close()
 
-        self.organize(self.folder)
+		with open('data.json', 'r') as f:
+			self.data = json.load(f)
+			f.close()
+		
+		if ok:
+			self.organize(self.folder)
 
-    def organize(self, folder):
-        files = os.listdir(folder)
-        if len(files) != 0:
-            for file in files:
-                ex = file.split('.')[-1]
-                for i in self.data:
-                    if ex in i['ext']:
-                        try:
-                            move(folder / file, folder / f'{i["name"]}/{file}')
-                        except FileNotFoundError:
-                            os.mkdir(folder / i['name'])
-                            move(folder / file, folder / f'{i["name"]}/{file}')
-                        break
+	def organize(self, folder):
+		files = os.listdir(folder)
+		if len(files) != 0:
+			for file in files:
+				ex = file.split('.')[-1]
+				for i in self.data:
+					if ex in i['ext']:
+						try:
+							move(folder / file, folder / f'{i["name"]}/{file}')
+						except FileNotFoundError:
+							os.mkdir(folder / i['name'])
+							move(folder / file, folder / f'{i["name"]}/{file}')
+						break
 
 if __name__ == '__main__':
-    App()
+	App()
