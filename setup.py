7cc496320a9621c9d212ad6ce5799bf5acf885ea
Unai Hernandez <unihernandez22@gmail.com>
2020-03-22 01:10:52 -0300

    Folder Organizer

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'folder-organizer',
+  packages = ['folder-organizer'],
+  version = '0.1',
+  description = 'my description',
+  author = 'Unai Minaberry y Abel ',
+  author_email = '',
+  url = 'https://github.com/unihernandez22/folder-organizer',
+  download_url = 'https://github.com/unihernandez22/folder-organizer',
+  keywords = ['organize', 'folders', 'files'],
+  classifiers = [],
+)
